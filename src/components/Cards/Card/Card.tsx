a3c9b9316cf54863d9bff7a51eed4c6b48576ad5
Bohdan Zots <Bohdan.Zots@quest.com>
2023-05-01 21:40:42 +0200

    Removed base url

--- src/components/Cards/Card/Card.tsx
+++ src/components/Cards/Card/Card.tsx
@@ -4,8 +4,6 @@ interface ICardProps {
   path: string;
 }
 
-const BASE_IMG_URL = '../../../assets/images/';
-
 export default function Card({ path }: ICardProps) {
   return <StyledCardImg src={require('../../../assets/images/' + path)} />;
 }
